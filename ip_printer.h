d3ce01aa630fa2132abc526e91b29851e1dd83d9
OlegWorld <osmironov@gmail.com>
2018-08-04 22:51:14 +0300

    single template

--- ip_printer.h
+++ ip_printer.h
@@ -2,15 +2,8 @@
 
 #include <iostream>
 #include <string>
+#include <tuple>
 #include <type_traits>
-
-template <template <typename, typename> typename Cont, typename T, typename Alloc>
-void print_ip(const Cont<T, Alloc>& ip) {
-    for (const auto& el : ip) {
-        std::cout << int(el) << (&el != &ip.back() ? "." : "");
-    }
-    std::cout << std::endl;
-}
 
 template <typename T>
 std::enable_if_t<std::is_same<T, std::string>::value> print_ip(const T& ip) {
@@ -25,3 +18,26 @@ std::enable_if_t<std::is_integral<T>::value> print_ip(const T& ip) {
     }
     std::cout << std::endl;
 }
+
+//template <template <typename, typename> typename Cont, typename T, typename Alloc>
+//void print_ip(const Cont<T, Alloc>& ip) {
+//    for (const auto& el : ip) {
+//        std::cout << int(el) << (&el != &ip.back() ? "." : "");
+//    }
+//    std::cout << std::endl;
+//}
+
+template <typename T>
+std::enable_if_t<!(std::is_same<T, std::string>::value) &&
+                 !(std::is_integral<T>::value)>
+                 print_ip(const T& ip) {
+    for (const auto& el : ip) {
+        std::cout << int(el) << (&el != &ip.back() ? "." : "");
+    }
+    std::cout << std::endl;
+}
+
+//template <typename... Args>
+//void print_ip(const std::tuple<Args...>& ip) {
+//
+//}
